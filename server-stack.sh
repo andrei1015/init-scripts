594a8aba79087ccbc2bfb62bdf4365cbb0cbb3dd
Andrei <andrei1015@gmail.com>
2022-05-24 13:17:26 +0200

    phpmyadmin

--- server-stack.sh
+++ server-stack.sh
@@ -3,11 +3,12 @@ sudo pacman --noconfirm -Sy $(pacman -Ssq php-)
 
 sudo sed -i 's/;sp.configuration_file/sp.configuration_file/g' /etc/php/conf.d/snuffleupagus.ini
 
-sudo pacman --noconfirm -Sy apache php-apache mariadb certbot certbot-apache
+sudo pacman --noconfirm -Sy apache php-apache mariadb phpmyadmin certbot certbot-apache
 
-sudo cp files/httpd.conf /etc/httpd/conf/httpd.conf
+sudo cp /home/arch/init-scripts/files/httpd.conf /etc/httpd/conf/httpd.conf
+sudo cp /home/arch/init-scripts/files/phpmyadmin.conf /etc/httpd/conf/extra/phpmyadmin.conf
 sudo chown -R root:root /etc/httpd/conf/httpd.conf
-sudo cp files/index.php /srv/http/index.php
+sudo cp /home/arch/init-scripts/files/index.php /srv/http/index.php
 
 sudo touch /etc/httpd/conf/extra/php-fpm.conf
 echo "DirectoryIndex index.php index.html" | sudo tee -a /etc/httpd/conf/extra/php-fpm.conf
@@ -22,13 +23,11 @@ sudo systemctl enable php-fpm --now
 sudo systemctl start httpd --now
 sudo systemctl enable httpd --now
 
-sudo mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 
 sudo rm /etc/my.cnf.d/server.cnf
 sudo cp /home/arch/init-scripts/files/server.cnf /etc/my.cnf.d/server.cnf
 
-sudo systemctl start mariadb.service
-sudo systemctl enable mariadb.service
-sudo systemctl restart mariadb.service
-
-# pacman --noconfirm -Sy phpmyadmin
+sudo systemctl start mariadb.service --now
+sudo systemctl enable mariadb.service --now
+sudo systemctl restart mariadb.service --now
