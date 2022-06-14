4719ed16a997af542d308b9351e14d57359023ce
Andrei <andrei1015@gmail.com>
2022-06-14 16:44:58 +0200

    Update aws-user-data-script.sh

--- aws-user-data-script.sh
+++ aws-user-data-script.sh
@@ -13,7 +13,7 @@ reflector --country "<ISO 3166-1 Alpha-2 Country Code>" --protocol https,http --
 pacman --noconfirm -Syy
 
 # MY PREFERENCE OF BASIC MUST HAVE PACKAGES, NEOFETCH INCLUDED OF COURSE
-pacman --noconfirm --needed -Sy base-devel git github-cli nano htop btop mc tmux python exa wget ncdu figlet zip unzip rsync lynx
+pacman --noconfirm --needed -Sy base-devel asciinema bat croc git github-cli micro nano htop btop mc tmux python exa wget ncdu figlet zip unzip rsync lynx
 
 # INSTALL YAY BECAUSE WHY EVEN USE PACMAN?
 mkdir /home/arch/yay
@@ -30,24 +30,25 @@ chown -R arch:arch /home/arch/.bash_history
 # DO STUFF IN .BASHRC
 echo "alias ea='exa -al --header --group --group-directories-first'"  >>  /home/arch/.bashrc
 echo "alias nano='nano --linenumbers --emptyline --mouse --indicator --magic'"  >>  /home/arch/.bashrc
-echo "alias ncdu='ncdu -q --confirm-quit --disable-delete --disable-shell --color dark'"  >>  /home/arch/.bashrc
 echo "alias cht='cht.sh'"  >>  /home/arch/.bashrc
+echo "alias clear='clear -x'"  >>  /home/arch/.bashrc
 echo "alias pixterm='pixterm -s 2'"  >>  /home/arch/.bashrc
 echo "alias lynx='lynx -accept_all_cookies -number_fields -number_links -use_mouse -scrollbar'"  >>  /home/arch/.bashrc
 echo "export EXA_COLORS='di=1;31:da=1;37:uu=1;31:gu=1;35:fi=1;37:sn=1;35'"  >>  /home/arch/.bashrc
-echo "export PS1='\[\e[0m\][\[\e[0;31m\]\u \[\e[0m\]@ \[\e[0;92m\]\w \[\e[0m\]@ \[\e[0;35m\]\t\[\e[0m\]] \[\e[0;97m\]\$ \[\e[0m\]'"  >>  /home/arch/.bashrc
+echo "export PS1='\[\e[0m\][\[\e[0;31m\]\u \[\e[0m\]@ \[\e[0;92m\]\w \[\e[0m\]@ \[\e[0;36m\]\t\[\e[0m\]] \[\e[0;97m\]\\$ \[\e[0;96m\]\$(git branch 2>/dev/null | grep '\"'\"'^*'\"'\"' | colrm 1 2) \[\e[0m\]'"  >>  /home/arch/.bashrc
 
 # MAKE YAY BEHAVE A BIT
 echo "yay --save --singlelineresults"  >>  /home/arch/.bashrc
 echo "yay --save --builddir /home/arch/yay"  >>  /home/arch/.bashrc
 
 # HISTORY SHENANIGANS IN BASHRC
-echo "export HISTSIZE=20000"  >>  /home/arch/.bashrc
+echo "export HISTSIZE=-1"  >>  /home/arch/.bashrc
+echo "export HISTFILESIZE=-1"  >>  /home/arch/.bashrc
 echo "export HISTCONTROL=erasedups"  >>  /home/arch/.bashrc
 echo "export HSTR_CONFIG=hicolor"  >>  /home/arch/.bashrc
 echo "export HSTR_CONFIG=raw-history-view"  >>  /home/arch/.bashrc
 echo "export HSTR_CONFIG=warning"  >>  /home/arch/.bashrc
-# echo "export PROMPT_COMMAND='history -a; history -n; ${PROMPT_COMMAND}'"  >>  /home/arch/.bashrc
+echo "export PROMPT_COMMAND='history -a; history -n; ${PROMPT_COMMAND}'"  >>  /home/arch/.bashrc
 
 # MAKE PACMAN/YAY BEAUTIFUL
 sed -ie '/^# Misc options/a Color' /etc/pacman.conf
@@ -55,10 +56,12 @@ sed -ie '/^# Misc options/a ILoveCandy' /etc/pacman.conf
 
 #YAY FUN
 touch /home/arch/aur-init-install.log
-runuser -l  arch -c 'yay --noconfirm -Sy micro pfetch hstr botsay pixterm cht.sh-git'  >> /home/arch/aur-init-install.log
+runuser -l  arch -c 'yay --noconfirm -Sy pfetch hstr botsay pixterm cht.sh-git'  >> /home/arch/aur-init-install.log
 
 # ADD BOTSAY TO MOTD FOR FUN OF COURSE
 
 dd if=/dev/null of=/etc/motd
 
 echo -e 'botsay -c "Welcome to 🐧 Arch btw!"' >> /home/arch/.bash_profile
+
+runuser -l  arch -c 'yay --noconfirm -Yc'
