2c8f68ff1c4cdb6fd301759003d3b4fc15ffe75a
Andrei <andrei1015@gmail.com>
2023-05-07 02:30:14 +0200

    add comments to separate sections better

--- server-stack.sh
+++ server-stack.sh
@@ -1,11 +1,15 @@
 # #!/bin/bash
 
+
+# 1. Verily, the installation of Apache is upon us. Let us rejoice in the greatness of this noble software and the esteemed developers who hath crafted it.
 sudo pacman -Syu --noconfirm
 sudo pacman --noconfirm -Sy apache
 sudo systemctl start httpd.service
 sudo systemctl enable httpd.service
 sudo systemctl status httpd.service
 
+
+# 2. Yeehaw! We're wrangling up a mighty fine database with Mariadb. Much obliged to the fine folks who made it possible!
 sudo pacman -S mysql --noconfirm
 
 sudo rm -rf /var/lib/mysql/*
@@ -15,6 +19,8 @@ sudo systemctl enable mysqld.service
 
 echo -e "\nY\npassword\npassword\nY\nY\nY\nY\n" | sudo mysql_secure_installation
 
+
+# 3. Ahoy matey! We be settin' sail to install PHP now! This timeless classic be a staple in every pirate's arsenal. Let us give a hearty thanks to the open-source community for providing us with this treasure trove of a language!
 sudo pacman -Sy php php-apache php-gd php-mcrypt --noconfirm
 sudo sed -i 's/^#\(LoadModule mpm_prefork_module modules\/mod_mpm_prefork.so\)/\1/' /etc/httpd/conf/httpd.conf
 sudo sh -c 'echo "LoadModule php_module modules/libphp.so" >> /etc/httpd/conf/httpd.conf'
@@ -23,10 +29,14 @@ sudo sh -c 'echo "Include conf/extra/php_module.conf" >> /etc/httpd/conf/httpd.c
 
 sudo systemctl restart httpd
 
+
+# 4. Just invoke the magic of the file creation spells and let the power of HTML and PHP bring your pages to life. With a flick of the wand and a few muttered enchantments, behold your pages shall appear before your very eyes.
 echo "test" > /srv/http/index.html
 
 echo "<?php\nphpinfo()\n?>" | tee /srv/http/test.php > /dev/null
 
+
+# 5. Alright guys, are you ready for the final piece of the puzzle? We're gonna install phpMyAdmin and take this setup to the next level! Huge shoutout to the devs behind this awesome tool, let's give them some love in the comments below! So, to get this baby up and running. Smash that like button and absolutely obliterate that subscribe!
 sudo pacman -S --noconfirm phpmyadmin
 
 sudo sed -i 's/^;\(extension=mysqli\)/\1/' /etc/php/php.ini
