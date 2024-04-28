10480a7e83f797befe59690ac3b392ccfea9bea3
Andrei <andrei1015@gmail.com>
2024-04-28 16:50:13 +0200

    to folder

--- aws-user-data-script.sh
+++ aws-user-data-script.sh
@@ -23,11 +23,10 @@ pacman --noconfirm --needed -Sy asciinema base-devel bat btop cockpit cockpit-st
 # ufw enable
 
 # 4. Me say use 🙌 paru for package management. Paru make everything easy-peasy like berries 🍓 on bush. No need hunt 🏹 and gather 🧺 for packages like Neanderthal. 🦕
-# runuser -l  arch -c 'mkdir /home/arch/paru'
-# cd /home/arch/paru
-runuser -l  arch -c 'git clone https://aur.archlinux.org/paru-bin.git'
-runuser -l  arch -c 'cd /home/arch/paru-bin; makepkg -si --noconfirm'
-
+runuser -l arch -c 'mkdir /home/arch/git'
+cd /home/arch/git
+runuser -l arch -c 'git clone https://aur.archlinux.org/paru-bin.git'
+runuser -l arch -c 'cd /home/arch/git/paru-bin; makepkg -si --noconfirm'
 
 # 5. I have just the sonic screwdriver 🔧 you need to fix history ⏰. With a simple flick of the wrist 💫 and a few choice commands 💻, we'll have that timeline back in ship-shape 🚀 in no time.
 touch /home/arch/.bash_history
@@ -36,26 +35,26 @@ chown -R arch:arch /home/arch/.bash_history
 
 # 6. Make your terminal a cozy home 🏠 with proper .bashrc configuration - let me show you how! 💻
 dd if=/dev/null of=/home/arch/.bashrc
-echo '[[ $- != *i* ]] && return'  >>  /home/arch/.bashrc
-echo -e 'mkcd() {\n    mkdir -p "$1" && cd "$1"\n}' >> /home/arch/.bashrc
-echo "alias pacz='pacman -Slq | fzf --multi --preview \"pacman -Si {1}\" | xargs -ro sudo pacman -S'" >> /home/arch/.bashrc
-echo "alias paruz='paru -Slq | fzf --multi --preview \"paru -Si {1}\" | xargs -ro paru -S'" >> /home/arch/.bashrc
-echo "alias ea='eza -al --header --group --group-directories-first'"  >>  /home/arch/.bashrc
-echo "alias nano='nano --linenumbers --emptyline --mouse --indicator --magic'"  >>  /home/arch/.bashrc
-echo "alias clear='clear -x'"  >>  /home/arch/.bashrc
-echo "alias vim='nvim'"  >>  /home/arch/.bashrc
-echo "alias pixterm='pixterm -s 2'"  >>  /home/arch/.bashrc
-echo "alias lynx='lynx -accept_all_cookies -number_fields -number_links -use_mouse -scrollbar'"  >>  /home/arch/.bashrc
-echo "export EXA_COLORS='di=1;31:da=1;37:uu=1;31:gu=1;35:fi=1;37:sn=1;35'"  >>  /home/arch/.bashrc
-echo "export PS1='\[\e[0m\][\[\e[0;31m\]\u \[\e[0m\]@ \[\e[0;92m\]\w \[\e[0m\]@ \[\e[0;36m\]\t\[\e[0m\]] \[\e[0;97m\]\\$ \[\e[0;96m\]\$(git branch 2>/dev/null | grep '\"'\"'^*'\"'\"' | colrm 1 2) \[\e[0m\]'"  >>  /home/arch/.bashrc
+echo '[[ $- != *i* ]] && return' >>/home/arch/.bashrc
+echo -e 'mkcd() {\n    mkdir -p "$1" && cd "$1"\n}' >>/home/arch/.bashrc
+echo "alias pacz='pacman -Slq | fzf --multi --preview \"pacman -Si {1}\" | xargs -ro sudo pacman -S'" >>/home/arch/.bashrc
+echo "alias paruz='paru -Slq | fzf --multi --preview \"paru -Si {1}\" | xargs -ro paru -S'" >>/home/arch/.bashrc
+echo "alias ea='eza -al --header --group --group-directories-first'" >>/home/arch/.bashrc
+echo "alias nano='nano --linenumbers --emptyline --mouse --indicator --magic'" >>/home/arch/.bashrc
+echo "alias clear='clear -x'" >>/home/arch/.bashrc
+echo "alias vim='nvim'" >>/home/arch/.bashrc
+echo "alias pixterm='pixterm -s 2'" >>/home/arch/.bashrc
+echo "alias lynx='lynx -accept_all_cookies -number_fields -number_links -use_mouse -scrollbar'" >>/home/arch/.bashrc
+echo "export EXA_COLORS='di=1;31:da=1;37:uu=1;31:gu=1;35:fi=1;37:sn=1;35'" >>/home/arch/.bashrc
+echo "export PS1='\[\e[0m\][\[\e[0;31m\]\u \[\e[0m\]@ \[\e[0;92m\]\w \[\e[0m\]@ \[\e[0;36m\]\t\[\e[0m\]] \[\e[0;97m\]\\$ \[\e[0;96m\]\$(git branch 2>/dev/null | grep '\"'\"'^*'\"'\"' | colrm 1 2) \[\e[0m\]'" >>/home/arch/.bashrc
 
 # 7. 🚂 All aboard! 🛤️ Add some history properties to your .bashrc file and keep track of your terminal's journey like a seasoned traveler. 🧳
-echo "export HISTSIZE=1000"  >>  /home/arch/.bashrc
-echo "export HISTFILESIZE=-1"  >>  /home/arch/.bashrc
-echo "export HISTCONTROL='erasedups:ignoredups'"  >>  /home/arch/.bashrc
-echo "export PROMPT_COMMAND='history -a'"  >>  /home/arch/.bashrc
-echo "export HISTTIMEFORMAT='%F %T '"  >>  /home/arch/.bashrc
-echo "source /usr/share/fzf/key-bindings.bash"  >>  /home/arch/.bashrc
+echo "export HISTSIZE=1000" >>/home/arch/.bashrc
+echo "export HISTFILESIZE=-1" >>/home/arch/.bashrc
+echo "export HISTCONTROL='erasedups:ignoredups'" >>/home/arch/.bashrc
+echo "export PROMPT_COMMAND='history -a'" >>/home/arch/.bashrc
+echo "export HISTTIMEFORMAT='%F %T '" >>/home/arch/.bashrc
+echo "source /usr/share/fzf/key-bindings.bash" >>/home/arch/.bashrc
 
 # 8. Don't settle for a basic package manager, hun! 💅 Let's put on some makeup and turn Pacman/paru into a fierce and fabulous superstar. 💄🌟 With custom configurations and colorful themes, your terminal will be serving looks for days! 👑
 sed -ie '/^# Misc options/a Color' /etc/pacman.conf
@@ -64,9 +63,8 @@ sed -ie '/^# Misc options/a VerbosePkgLists' /etc/pacman.conf
 
 # 9. Upgrade your terminal game with paru and get the hottest apps in town! 🚀🔥 From productivity to gaming, we've got it all. Let's roll! 🎮💻
 touch /home/arch/aur-init-install.log
-runuser -l  arch -c 'paru --noconfirm -S pfetch botsay pixterm cockpit-navigator'  >> /home/arch/aur-init-install.log
+runuser -l arch -c 'paru --noconfirm -S pfetch botsay pixterm cockpit-navigator' >>/home/arch/aur-init-install.log
 rm /var/lib/pacman/db.lck
-
 
 systemctl start cockpit.socket
 systemctl enable cockpit.socket
@@ -77,7 +75,7 @@ systemctl start NetworkManager
 
 dd if=/dev/null of=/etc/motd
 
-echo -e 'botsay -c "Welcome to Arch btw!"' >> /home/arch/.bash_profile
+echo -e 'botsay -c "Welcome to Arch btw!"' >>/home/arch/.bash_profile
 
 echo "+++++++++++++++++++++++++++++++++++++++++++++++"
 echo "++++++++++++++++SCRIPT END+++++++++++++++++++++"
